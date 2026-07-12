cda08cd8ca5d9679cbd645d076e95717432cfa5b
Venu Sai Madisetti <venu.madisetti@freshgravity.com>
2026-07-12 17:53:04 +0530

    new website.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    async rewrites() {
+        return [{source: "/resume", destination: "/docs/resume.pdf"}];
+    },
 };
 
 export default nextConfig;
